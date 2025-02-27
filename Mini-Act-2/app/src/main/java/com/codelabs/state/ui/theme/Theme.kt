2a07f204555a997918b21e6669ef0dd3e0c5a373
Marc "Jakon" Lapeña <jakoncoach@gmail.com>
2025-02-27 09:33:01 +0100

    Update Mini-Act-2 (default file update)

--- Mini-Act-2/app/src/main/java/com/codelabs/state/ui/theme/Theme.kt
+++ Mini-Act-2/app/src/main/java/com/codelabs/state/ui/theme/Theme.kt
@@ -1,4 +1,19 @@
-package com.example.mini_act_2.ui.theme
+/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.codelabs.state.ui.theme
 
 import android.app.Activity
 import android.os.Build
@@ -9,7 +24,11 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -34,7 +53,7 @@ private val LightColorScheme = lightColorScheme(
 )
 
 @Composable
-fun MiniAct2Theme(
+fun BasicStateCodelabTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
@@ -48,6 +67,14 @@ fun MiniAct2Theme(
 
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
+    }
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = colorScheme.primary.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+        }
     }
 
     MaterialTheme(
