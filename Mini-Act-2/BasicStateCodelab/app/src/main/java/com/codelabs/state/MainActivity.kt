e8f1a978192c69ba80e2d080a2282aa97db10451
Marc "Jakon" Lapeña <jakoncoach@gmail.com>
2025-03-01 13:03:14 +0100

    Step 4: Added Button (not working yet)

--- Mini-Act-2/BasicStateCodelab/app/src/main/java/com/codelabs/state/MainActivity.kt
+++ Mini-Act-2/BasicStateCodelab/app/src/main/java/com/codelabs/state/MainActivity.kt
@@ -18,8 +18,10 @@ package com.codelabs.state
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -64,11 +66,13 @@ fun GreetingPreview() {
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
-    val count = 0
-    Text(
-        text = "You've had $count glasses.",
-        modifier = modifier.padding(16.dp)
-    )
+    Column(modifier = modifier.padding(16.dp)) {
+        var count = 0
+        Text("You've had $count glasses.")
+        Button(onClick = { count++ }, Modifier.padding(top = 8.dp)) {
+            Text("Add one")
+        }
+    }
 }
 
 @Composable
