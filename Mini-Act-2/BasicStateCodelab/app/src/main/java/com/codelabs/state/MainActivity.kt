a54ee5be2b909e4670c28caeb9778e6a6c367c25
Marc "Jakon" Lapeña <jakoncoach@gmail.com>
2025-03-01 13:02:06 +0100

    Step 3:Added WaterCounter and WellnessScreen

--- Mini-Act-2/BasicStateCodelab/app/src/main/java/com/codelabs/state/MainActivity.kt
+++ Mini-Act-2/BasicStateCodelab/app/src/main/java/com/codelabs/state/MainActivity.kt
@@ -19,12 +19,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.codelabs.state.ui.theme.BasicStateCodelabTheme
 
 class MainActivity : ComponentActivity() {
@@ -37,7 +39,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    WellnessScreen()
                 }
             }
         }
@@ -58,4 +60,18 @@ fun GreetingPreview() {
     BasicStateCodelabTheme {
         Greeting("Android")
     }
+}
+
+@Composable
+fun WaterCounter(modifier: Modifier = Modifier) {
+    val count = 0
+    Text(
+        text = "You've had $count glasses.",
+        modifier = modifier.padding(16.dp)
+    )
+}
+
+@Composable
+fun WellnessScreen(modifier: Modifier = Modifier) {
+    WaterCounter(modifier)
 }
