8227be3247f4d8e557291b9073708e60cc98eada
Marc "Jakon" Lapeña <jakoncoach@gmail.com>
2025-03-01 13:30:01 +0100

    Step 9: Refactor of WaterCounter - Split in 2 parts

--- Mini-Act-2/BasicStateCodelab/app/src/main/java/com/codelabs/state/MainActivity.kt
+++ Mini-Act-2/BasicStateCodelab/app/src/main/java/com/codelabs/state/MainActivity.kt
@@ -15,12 +15,10 @@
  */
 package com.codelabs.state
 
-import WellnessTaskItem
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
@@ -31,7 +29,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -72,19 +69,26 @@ fun GreetingPreview() {
 }
 
 @Composable
-fun WaterCounter(modifier: Modifier = Modifier) {
+fun StatelessCounter(count: Int, onIncrement: () -> Unit, modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
-        var count by rememberSaveable { mutableStateOf(0) }
         if (count > 0) {
             Text("You've had $count glasses.")
         }
-        Button(onClick = { count++ }, Modifier.padding(top = 8.dp), enabled = count < 10) {
+        Button(onClick = onIncrement, Modifier.padding(top = 8.dp), enabled = count < 10) {
             Text("Add one")
         }
     }
 }
 
 @Composable
+fun StatefulCounter(modifier: Modifier) {
+    var count by remember { mutableStateOf(0) }
+
+    StatelessCounter(count, { count++ })
+}
+
+
+@Composable
 fun WellnessScreen(modifier: Modifier = Modifier) {
-    WaterCounter(modifier)
+    StatefulCounter(modifier)
 }
